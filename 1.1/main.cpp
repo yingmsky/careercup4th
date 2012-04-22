a39780e176d62f521efc4833e2689297bb6c5eb2
Jian Gong <gboystal@gmail.com>
2012-04-22 23:29:45 +0800

    modify annotation

--- 1.1/main.cpp
+++ 1.1/main.cpp
@@ -1,7 +1,8 @@
 /*	
 	CareerCup 150 Questions 4th
 	Solutions to Chapter 1 | Arrays and Strings
-	1.1 Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures? [VS2010]
+	1.1 Implement an algorithm to determine if a string has all unique characters.
+		What if you can not use additional data structures? [VS2010]
 */
 
 #include<iostream>

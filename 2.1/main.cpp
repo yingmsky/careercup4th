ea40f4b6f6e80f6b57f9b6d2cd6b8b5c23fdebc9
Jian Gong <gboystal@gmail.com>
2012-04-26 16:28:17 +0800

    modify annotation

--- 2.1/main.cpp
+++ 2.1/main.cpp
@@ -1,7 +1,7 @@
 /*	
     CareerCup 150 Questions 4th
     Solutions to Chapter 2 | Linked Lists
-    1.8 Write code to remove duplicates from an unsorted linked list.
+    2.1 Write code to remove duplicates from an unsorted linked list.
         FOLLOW UP
         How would you solve this problem if a temporary buffer is not allowed?
     [VS2010][2012.04.26][by gboystal]

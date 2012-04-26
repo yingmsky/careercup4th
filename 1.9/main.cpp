4365ab8ffae6ccf27127ed3a29c8c11153839e84
Jian Gong <gboystal@gmail.com>
2012-04-26 16:24:20 +0800

    add hash method

--- 1.9/main.cpp
+++ 1.9/main.cpp
@@ -10,6 +10,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<map>
 using namespace std;
 
 void deleteDups(vector<int>& v)
@@ -29,11 +30,30 @@ void deleteDups(vector<int>& v)
     while((len--) != newsize) v.pop_back();
 }
 
+void deleteDups2(vector<int>& v)
+{
+    int len = v.size();
+    if(len < 2) return;
+    
+    map<int,int> hash;
+    
+    int newsize = 0;
+    for(int i = 0; i < len; i++)
+    {
+        if(hash.count(v[i]) == 0)
+        {
+            hash[v[i]] = 1;
+            v[newsize++] = v[i];
+        }
+    }
+    while((len--) != newsize) v.pop_back();
+}
+
 int main()
 {
     int myints [] = {10, 20, 30, 30, 20, 10, 10, 20};
     vector<int> v(myints, myints + sizeof(myints) / sizeof(int));
-    deleteDups(v);
+    deleteDups2(v);
     for(vector<int>::iterator it = v.begin(); it != v.end(); it++)
         cout<<*it<<" ";
     return 0;
